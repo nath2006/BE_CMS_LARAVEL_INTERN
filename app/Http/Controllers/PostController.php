fdf52f9af571de38a94a7c76c09a522a51e7adfe
Nath <ananthapindika739@gmail.com>
2023-08-12 23:49:09 +0700

    update create, update and deleted method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,4 +29,35 @@ public function index() {
         // Create a new PostResource instance and pass the Post model to it
         return new PostResource($post);
     }
+
+    public function update(Request $request, $id) {
+        try {
+            $post = Post::findOrFail($id);
+    
+            $validatedData = $request->validate([
+                'title'        => 'required|max:255',
+                'post_content' => 'required',
+                'img_link'     => 'nullable', // Anda dapat menggunakan nullable jika ingin mengizinkan img_link kosong
+            ]);
+    
+            $post->fill($validatedData);
+            $post->save();
+    
+            return new PostResource($post);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Update failed'], 500);
+        }
+    }
+    
+    public function destroy($id) {
+        try {
+            $post = Post::findOrFail($id);
+            $post->delete();
+    
+            return response()->json(['message' => 'Post deleted successfully']);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Delete failed'], 500);
+        }
+    }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/posts',[PostController::class, 'index']);
 Route::post('/posts', [PostController::class, 'store']);
+Route::patch('/posts/{id}', [PostController::class, 'update']);
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
